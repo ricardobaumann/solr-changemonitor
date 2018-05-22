624d22dfca747e47419301d0840da02ac792d2b3
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2018-05-22 15:54:02 +0200

    including lquibase support

--- src/main/java/com/github/ricardobaumann/solrchangemonitor/model/ChangeBatch.java
+++ src/main/java/com/github/ricardobaumann/solrchangemonitor/model/ChangeBatch.java
@@ -3,21 +3,28 @@ package com.github.ricardobaumann.solrchangemonitor.model;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
+@Table(name = "change_batch")
 public class ChangeBatch {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "last_modified_date")
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastModifiedDate;
+
+    @Column(name = "created_at")
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
 
